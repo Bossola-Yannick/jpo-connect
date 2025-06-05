a641cc86b3d6294c4f895ca0069c5cf4b8dfe804
Bossola-Yannick <bossolay@hotmail.fr>
2025-06-05 12:11:40 +0200

    feat: - Refactored App.jsx to implement React Router for navigation and removed unnecessary state management.

---
+++ jpo_connect_front/src/pages/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "../components/Button/Button";
+
+export default function Homepage() {
+  return (
+    <>
+      <Button className="start-button">clique ici</Button>
+      <h1>Hello</h1>
+    </>
+  );
+}
