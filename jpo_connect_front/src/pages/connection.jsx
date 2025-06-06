b8ed4e3ee38ef9e1471d8f2dce88fd661137ff18
Bossola-Yannick <bossolay@hotmail.fr>
2025-06-06 12:02:25 +0200

    feat: added fornt admin page and css associate

--- jpo_connect_front/src/pages/connection.jsx
+++ jpo_connect_front/src/pages/connection.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import "./connection-inscription.css";
+// import "../js/verif-input.js";
 import Button from "../components/Button/Button";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function Connection() {
   return (
-    <>
-      <Button className="start-button">Se Connecté</Button>
-      <h1>Suis-je ou ne suis-je pas moi</h1>
-    </>
+    <section className="connection">
+      <h1 className="connection-title">Connexion</h1>
+      <form method="post" action="" id="form-connection">
+        <label for="email">Votre Email</label>
+        <input type="email" name="email" id="email" />
+        <span id="emailError"></span>
+        <label for="password">Votre Mot de Passe</label>
+        <input type="password" name="password" id="password" />
+        <Button className="button-registration">Connexion</Button>
+        <span id="connection-message"></span>
+      </form>
+      <p className="text-info">
+        Si vous n'avez pas de compte veuillez vous{" "}
+        <Link to="/inscription" className="text-info">
+          Inscrire
+        </Link>
+      </p>
+    </section>
   );
 }
